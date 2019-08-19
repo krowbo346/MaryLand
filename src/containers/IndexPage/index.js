399e70535612988e9101655f20b2c7418ab31cbe
Mary Li <krowbo346@gmail.com>
2019-08-19 20:41:26 +0800

    background

--- src/containers/IndexPage/index.js
+++ src/containers/IndexPage/index.js
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import styled from 'styled-components';
 
 const Card = styled.div`
-  width: 480px;
-  height: 640px;
-  border: solid 1px black;
+  width: 400px;
+  height: 600px;
+  border-radius: 30px;
+  background-color: hsl(215, 62%, 21%);
 `;
 
 const A = styled.a``;
@@ -26,9 +27,7 @@ const Wrapper = styled.div`
 const IndexPage = () => (
   <Wrapper>
     <Card />
-    <Link href='/about' passHref>
-      <A>About Page</A>
-    </Link>
+    
   </Wrapper>
 );
 
