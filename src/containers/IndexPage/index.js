08379354357bc20a4fc845c721bbadf6d6cee316
Mary Li <krowbo346@gmail.com>
2019-08-19 21:14:41 +0800

    NameCard_finished

--- src/containers/IndexPage/index.js
+++ src/containers/IndexPage/index.js
@@ -1,30 +1,44 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
+const EkkImage = styled.img``;
+
 const Card = styled.div`
   width: 400px;
   height: 600px;
   border-radius: 30px;
   background-color: hsl(215, 62%, 21%);
+  position: relative;
+  overflow: hidden;
+  ${EkkImage} {
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+  }
 `;
 
 const Logo = styled.img`
   margin-top: 20px;
-  margin-left: 20px;
+  margin-left: 0px;
 `;
 
 const ChurchText = styled.p`
+  margin-top: 20px;
+  margin-left: 20px;
   font-size: 40px;
   text-align: center;
   color: white;
 `;
 
 const NameText = styled.p`
-  font-size: 60px;
+  font-size: 80px;
   text-align: center;
-  letter-spacing: 20px;
-  text-indent: 20px;
+  font-weight: 800;
+  letter-spacing: 30px;
+  text-indent: 30px;
   color: white;
+  margin-top: 20px;
+  margin-left: 20px;
 `;
 
 const A = styled.a``;
@@ -49,6 +63,7 @@ const IndexPage = () => (
       <Logo src='/static/logo.svg' />
       <ChurchText>新生命小組教會</ChurchText>
       <NameText>李欣美</NameText>
+      <EkkImage src='/static/ekk.svg' />
     </Card>
   </Wrapper>
 );
